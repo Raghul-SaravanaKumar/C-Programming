c5f9b1b707cd8d9d3bcfae6ed76a6e84338552be
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-09-01 22:59:23 +0530

    Fix formatting and remove unnecessary lines

--- PlaywithCharacters.c
+++ PlaywithCharacters.c
@@ -2,13 +2,11 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-
 int main() 
 {
-
-   char ch;
-   char s[100];
-   char s2[100];
+char ch;
+char s[100];
+char s2[100];
    
 scanf("%c", &ch);
 scanf("%s", &s);
